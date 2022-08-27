239152792ae1ab26609b6f89a468054efe6f3ad3
Timophey <ulezko.2015@mail.ru>
2022-08-27 03:19:57 +0300

    minor changes

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ document.querySelectorAll('a[href=""]').forEach(a => {
     console.log(a);
     a.addEventListener('click', e => e.preventDefault());
 });
+
+// custom checkbox
+document.querySelectorAll('.checkbox').forEach(c => {
+    c.addEventListener('click', e => {
+        e.target.classList.toggle('active');
+    });
+})
